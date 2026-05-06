7cecca27140246f894f7eb7acdfb634c45b098d2
RosemaryZL <rosemaryzaldana@gmail.com>
2026-05-05 20:40:50 -0600

    Limpieza de archivos duplicados

--- src/Storage/StorageManager.php
+++ src/Storage/StorageManager.php
@@ -15,11 +15,32 @@ class StorageManager {
     // GUARDAR ARCHIVO
     public function guardarArchivo($archivo) {
 
+        // ✔ EXTENSIONES PERMITIDAS
+        $extensionesPermitidas = [
+            'jpg', 'jpeg', 'png', 'gif', 'bmp', 'webp', 'ico',
+            'pdf','zip', 'rar', '7z', 'tar', 'gz','mp3', 'mp4', 'wav', 'avi'
+        ];
+
+        $extension = strtolower(pathinfo($archivo['name'], PATHINFO_EXTENSION));
+
+        if (!in_array($extension, $extensionesPermitidas)) {
+            throw new Exception("Extensión no permitida");
+        }
+
+        // ✔ VALIDAR TAMAÑO (5MB)
+        $maxSize = 5 * 1024 * 1024;
+
+        if ($archivo['size'] > $maxSize) {
+            throw new Exception("Archivo demasiado grande (máx 5MB)");
+        }
+
+        // ✔ NOMBRE SEGURO
         $nombreSeguro = preg_replace('/[^a-zA-Z0-9._-]/', '_', $archivo['name']);
         $nombreUnico = uniqid() . "_" . $nombreSeguro;
 
         $rutaFinal = $this->directorio . $nombreUnico;
 
+        // ✔ MOVER ARCHIVO
         if (move_uploaded_file($archivo['tmp_name'], $rutaFinal)) {
             return $rutaFinal;
         }
@@ -27,7 +48,7 @@ class StorageManager {
         throw new Exception("Error al guardar el archivo");
     }
 
-    //  ELIMINAR ARCHIVO FÍSICO 
+    // ✔ ELIMINAR ARCHIVO FÍSICO
     public function eliminarArchivo($ruta) {
 
         if (file_exists($ruta)) {
