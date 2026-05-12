5f10ce78b4f5ed2aeab9a0d9b6b0dac369ac4aa1
Eunice-projects <beltraneunice05@gmail.com>
2026-05-12 17:32:36 -0600

    Acualizando estilos en index.php y editando theme.css

--- public/index.php
+++ public/index.php
@@ -65,11 +65,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <head>
     <meta charset="UTF-8">
-    <title>AnalizadorFirmas — Prueba</title>
+    <title>AnalizadorFirmas</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/theme.css">
 </head>
 <!-- Elimine la etiqueta <style> de aca y lo pase a assets/css/theme.css para llevar un orden-->
+
 <body>
     <div class="container">
         <a class="api-links" href="logout.php">Cerrar sesión</a>
@@ -79,12 +80,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <form method="POST" enctype="multipart/form-data">
                 <input type="file" name="archivo" required>
                 <br>
-                <button class="button btn-upload" type="submit">Analizar archivo</button>
+                <button class="btn-success" type="submit">Analizar archivo</button>
             </form>
 
             <?php if ($resultado): ?>
-                <div class="resultado">
-                    <h3>✅ Archivo analizado correctamente</h3>
+            <div class="resultado">
+                <h3>✅ Archivo analizado correctamente</h3>
+
+                <!-- Contenedor para controlar el espacio -->
+                <div class="tabla-contenedor">
                     <table>
                         <tr>
                             <td>Nombre</td>
@@ -93,12 +97,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <tr>
                             <td>Tipo detectado</td>
                             <td><strong><?= htmlspecialchars($resultado['tipo']) ?></strong> (código
-                                <?= $resultado['codigo'] ?>)
-                            </td>
+                                <?= $resultado['codigo'] ?>)</td>
                         </tr>
                         <tr>
                             <td>Hash MD5</td>
-                            <td><?= $resultado['hash'] ?></td>
+                            <td><code><?= $resultado['hash'] ?></code></td>
                         </tr>
                         <tr>
                             <td>Tamaño</td>
@@ -106,10 +109,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         </tr>
                     </table>
                 </div>
+            </div>
             <?php endif; ?>
 
+
             <?php if ($error): ?>
-                <div class="error">❌ Error: <?= htmlspecialchars($error) ?></div>
+            <div class="error">❌ Error: <?= htmlspecialchars($error) ?></div>
             <?php endif; ?>
             <?php
             $conexion = Conexion::getInstance();
@@ -117,7 +122,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $archivos = $repo->obtenerTodos();
             ?> <h3> Historial de archivos</h3>
 
-            <table >
+            <table>
                 <tr>
                     <th>ID</th>
                     <th>Nombre</th>
@@ -127,18 +132,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </tr>
 
                 <?php foreach ($archivos as $a): ?>
-                    <tr>
-                        <td><?= $a['id'] ?></td>
-                        <td><?= htmlspecialchars($a['nombre_original']) ?></td>
-                        <td><?= $a['tipo_detectado'] ?></td>
-                        <td><?= $a['tamaño'] ?></td>
-                        <td><?= $a['fecha_subida'] ?></td>
-                    </tr>
+                <tr>
+                    <td><?= $a['id'] ?></td>
+                    <td><?= htmlspecialchars($a['nombre_original']) ?></td>
+                    <td><?= $a['tipo_detectado'] ?></td>
+                    <td><?= $a['tamaño'] ?></td>
+                    <td><?= $a['fecha_subida'] ?></td>
+                </tr>
                 <?php endforeach; ?>
             </table>
 
             <div class="api-links">
-                <strong>Endpoints API REST:</strong><br>
+                <strong class="endpoints-title">Endpoints API REST:</strong><br>
                 <a class="api-links" href="api/tipos.php">GET /api/tipos</a>
                 <a class="api-links" href="api/version.php">GET /api/version</a>
                 <span style="color:#666">POST /api/analizar</span>
